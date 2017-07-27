2f9c5af694c7e31f26b0d45c23aec07a0c72204c
JD Leshan <stritch@gmail.com>
2017-07-27 17:26:08 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc array
+  array.sort
+end
 
+def sort_array_desc array
+  array.sort.reverse
+end
+
+def sort_array_char_count array
+  array.sort_by do |word|
+    word.length
+  end
+end
+
+def swap_elements array
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
+
+def reverse_array array
+  array.reverse
+end
+
+def kesha_maker array
+  result = []
+  array.each do |word|
+    result << word[0..1] + "$" + word[3..word.length]
+  end
+  result
+end
+
+def find_a array
+  array.select do |word|
+    word.start_with?('a')
+  end
+end
+
+# help from https://apidock.com/ruby/Enumerable/inject
+def sum_array array
+  # array.reduce(:+)
+  array.inject do |sum, n| sum + n end
+end
+
+def add_s array
+  array.each_with_index do |word, index|
+    array[index] = array[index] + "s" unless index == 1
+  end
+end
